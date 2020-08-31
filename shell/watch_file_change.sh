30a89b07a2e6da8c549add44194c2b08710f9df3
chen <meichen_wu@163.com>
2020-08-31 15:27:50 +0800

    --

--- shell/watch_file_change.sh
+++ shell/watch_file_change.sh
@@ -4,13 +4,17 @@ run_path="/root/"
 bk_path="/root/jar_bk/"
 log="watch.log"
 JARNAME="demo-0.0.1-SNAPSHOT.jar"
+JARNAME2="web-0.0.1-SNAPSHOT.jar"
 inotifywait -mr --timefmt '%d/%m/%y %H:%M' --format '%T %w %f' -e close_write $src | while read DATE TIME DIR FILE; do
  
 FILECHANGE=${DIR}${FILE}
  
-if [ $FILE == $JARNAME ]
-then
-        echo “At ${TIME} on ${DATE}, file $FILECHANGE was change” >> $log
-        /root/ops/shell/springboot.sh $FILE stop >> $log && mv ${run_path}${JARNAME} ${bk_path}${JARNAME}'.'$(date +'%Y%m%d') >> $log  && cp  ${src}${JARNAME} ${run_path} >> $log && /root/ops/shell/springboot.sh $FILE start >> $log
-fi
+case "$FILE" in
+$JARNAME|$JARNAME2)
+     echo “At ${TIME} on ${DATE}, file $FILECHANGE was change” >> $log
+    /root/ops/shell/springboot.sh $FILE stop >> $log && mv ${run_path}${FILE} ${bk_path}${FILE}'.'$(date +'%Y%m%d') >> $log  && cp  ${src}${FILE} ${run_path} >> $log && /root/ops/shell/springboot.sh $FILE start >> $log
+     ;;
+*)
+   exit 1
+esac
 done
